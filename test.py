dc194972f8a83d17c7adead7d18f8ef0e560bc74
Christophe Willaert <christophe.willaert11@gmail.com>
2020-08-14 22:07:21 +0200

    Setting environment on cwl-dt, added test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version)
+
+name = input("naam: ")
+print("Hello ",name)
+
+#print(os.environ.keys)
